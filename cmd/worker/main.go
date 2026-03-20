02225885cbd13cb3d96f35a63eafe55710e4339b
kachelyass <kachelyass123@gmail.com>
2026-03-20 19:55:59 +0300

    change main for quiality

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"context"
 	"log"
-	"sync"
 	"time"
-
+	"worker_pool/internal/WorkerPool"
 	"worker_pool/internal/infrastructure/postgre"
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	db, err := postgre.Connect(ctx, "postgresql://user:pass@localhost:5432/db?sslmode=disable")
 	if err != nil {
@@ -19,45 +19,17 @@ func main() {
 	defer db.Close()
 
 	store := postgre.NewTaskStore(db)
+	handler := WorkerPool.NewJobHandler(store)
 
-	ids, err := store.GetPendingIDs(ctx)
+	limit := 10
 
-	const numWorkers = 5
-	jobs := make(chan int, len(ids))
+	jobs := make(chan WorkerPool.JobTask, limit)
 
-	var wg sync.WaitGroup
-	wg.Add(len(ids))
-
-	// старт воркеров
-	for w := 0; w < numWorkers; w++ {
-		go worker(w, jobs, store, ctx, &wg)
+	for w := 1; w <= limit; w++ {
+		go handler.Worker(ctx, w, jobs)
 	}
 
-	// отправляем задачи
-	for _, id := range ids {
-		jobs <- id
-	}
-	close(jobs)
-
-	// ждём, пока все задачи будут обработаны
-	wg.Wait()
-	log.Println("all pending tasks processed")
-}
-
-func worker(workerID int, jobs <-chan int, store *postgre.TaskStore, ctx context.Context, wg *sync.WaitGroup) {
-	for taskID := range jobs {
-		log.Printf("worker %d starting job %d", workerID, taskID)
-
-		time.Sleep(8 * time.Second)
-
-		_, err := store.UpdateTaskStatus(ctx, taskID)
-		if err != nil {
-			log.Printf("worker %d error on job %d: %v", workerID, taskID, err)
-			wg.Done()
-			continue
-		}
-
-		log.Printf("worker %d finishing job %d", workerID, taskID)
-		wg.Done()
+	for j := 1; j <= limit; j++ {
+		handler.Producer(ctx, jobs, j)
 	}
 }
