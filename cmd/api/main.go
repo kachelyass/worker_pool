d66401ed51ad017a9da5be2aa093ee6cce0c57f8
kachelyass <kachelyass123@gmail.com>
2026-03-20 15:00:05 +0300

    Restore cmd/api/main.go to 9aeeab66

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"context"
 	"log"
-	"sync"
-	"time"
-
+	"net/http"
+	"strconv"
+	"strings"
+	"worker_pool/internal/handlers"
 	"worker_pool/internal/infrastructure/postgre"
 )
 
 func main() {
 	ctx := context.Background()
-
 	db, err := postgre.Connect(ctx, "postgresql://user:pass@localhost:5432/db?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
@@ -19,53 +19,38 @@ func main() {
 	defer db.Close()
 
 	store := postgre.NewTaskStore(db)
+	taskHandler := handlers.NewTaskHandler(store)
+	mux := http.NewServeMux()
 
-	ids, err := store.GetPendingIDs(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("pending tasks: %d", len(ids))
-	if len(ids) == 0 {
-		return
-	}
-
-	const numWorkers = 5
-	jobs := make(chan int, len(ids)) // буфер под все задачи
-
-	var wg sync.WaitGroup
-	wg.Add(len(ids)) // будем “Done” на каждую обработанную задачу
-
-	// старт воркеров
-	for w := 0; w < numWorkers; w++ {
-		go worker(w, jobs, store, ctx, &wg)
-	}
-
-	// отправляем задачи
-	for _, id := range ids {
-		jobs <- id
-	}
-	close(jobs) // важно: чтобы воркеры вышли из range
-
-	// ждём, пока все задачи будут обработаны
-	wg.Wait()
-	log.Println("all pending tasks processed")
-}
-
-func worker(workerID int, jobs <-chan int, store *postgre.TaskStore, ctx context.Context, wg *sync.WaitGroup) {
-	for taskID := range jobs {
-		log.Printf("worker %d starting job %d", workerID, taskID)
-
-		time.Sleep(20 * time.Second)
-
-		// ВАЖНО: обновляем статус задачи по taskID
-		_, err := store.UpdateTaskStatus(ctx, taskID)
-		if err != nil {
-			log.Printf("worker %d error on job %d: %v", workerID, taskID, err)
-			wg.Done()
-			continue
+	mux.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			taskHandler.GetALl(w, r)
+		case http.MethodPost:
+			taskHandler.Create(w, r)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
+	})
 
-		log.Printf("worker %d finishing job %d", workerID, taskID)
-		wg.Done()
-	}
+	mux.HandleFunc("/tasks/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			idStr := strings.TrimPrefix(r.URL.Path, "/tasks/")
+			id, err := strconv.Atoi(idStr)
+			if err != nil {
+				http.Error(w, "Invalid task ID", http.StatusBadRequest)
+				return
+			}
+			taskHandler.GetByID(w, r, id)
+		case http.MethodPost:
+
+			taskHandler.Create(w, r)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+
+	log.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
